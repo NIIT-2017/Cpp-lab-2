71f1e7649cea2de925e5b5a4b0e57a1e091bcd87
YakovDeLeon <swimmerfromrussia@gmail.com>
2018-08-01 02:30:43 +0300

    Добавил решение

---
+++ include/task2.h
@@ -0,0 +1,31 @@
+#pragma once
+
+template<class T>
+T gen()
+{
+	static int t = 48;
+	return t++;
+}
+
+template<>
+char* gen()
+{
+	static unsigned t = 0;
+	t++;
+	char* s = new char[t + 1];
+
+	for (size_t i = 0; i < t; i++)
+		s[i] = '0' + i;
+	s[t] = '\0';
+
+	return s;
+}
+
+template<typename T, size_t N = 50>
+T * createArr(T (*gen)(void))
+{
+	T * arr = new T[N];
+	for (size_t i = 0; i < N ; ++i)
+		arr[i] = (*gen)();
+	return arr;
+}
