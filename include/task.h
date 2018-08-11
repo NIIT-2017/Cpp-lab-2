63516d6122aedc7efa8176fa21db76cd8f4f6728
Anna Pershina <7110575@mail.ru>
2018-08-11 13:14:21 +0300

    4

--- include/task.h
+++ include/task.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstring>
+#include <cstdlib>
 #include <iostream>
 template<typename T>
 T gen()
@@ -16,19 +18,17 @@ char* gen()
 	s[t] = '\0';
 	return s;
 }
-#include <cstring>
-#include <cstdlib>
 
-template<class T>
-T change(T a)
-{
-	a = (T)(rand());
-	return a;
-}
-template<> char* change(char* str)
-{
-	size_t n = strlen(str);
-	for (size_t i = 0; i < n; i++)
-		str[i] = (char)(rand());
-	return str;
-}
+
+//template<class T> T change(T a)
+//{
+//	a = (T)(rand());
+//	return a;
+//}
+//template<> char* change(char* str)
+//{
+//	size_t n = strlen(str);
+//	for (size_t i = 0; i < n; i++)
+//		str[i] = (char)(rand());
+//	return str;
+//}
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,12 +1,12 @@
 #include "task3.h"
-#include "task.h"
+//#include "task.h"
 #include <iostream>
 using namespace std;
 int main()
 {
 	const size_t n = 3;
 	int arr1[n]{ 1, 2, 3};
-	map<int, n>(arr1, change);
+    map<int, n>(arr1,name::change);
 	for (int i = 0; i < n; i++)
 		cout << arr1[i] << " ";
 	cout << endl;
