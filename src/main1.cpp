55fdab84e5f00ed12d038800dbca93df2c5e1ec7
Victoria <alvi813@yandex.ru>
2018-08-22 20:18:20 +0300

    fixed a bug with skipping letters

--- src/main1.cpp
+++ src/main1.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	const size_t n = 10;
 	int* value1 = new int[n]{ 99, 5, 6, 3, 2, 51, 24, 73, 60, 13 };
-	msort(value, n);
-	print(value, n);
+	msort(value1, n);
+	print(value1, n);
 	
 	
 	double value2[n]{ 5.1, 78.6, 33.4, 52.5, 71.3, 28, -13, -66.4, 6.7, -12.9 };
@@ -20,6 +20,6 @@ int main()
 	msort(value3, n);
 	print(value3, n);
 	
-	delete[] value;
+	delete[] value1;
 	return 0;
 }
