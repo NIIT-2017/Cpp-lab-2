550e032e1184a9d4fb471272f9b3aca7987466ad
Anton Sorokin <Tofer159@gmail.com>
2018-08-25 09:01:26 +0300

    v12

---
+++ include/task.h
@@ -0,0 +1,20 @@
+template<class T>
+T gen()
+{
+	static int t = 48;
+	return t++;
+}
+
+template<>
+char* gen()
+{
+	static unsigned t = 0;
+	t++;
+	char* s = new char[t + 1];
+
+	for (size_t i = 0; i < t; i++)
+		s[i] = '0' + i;
+	s[t] = '\0';
+
+	return s;
+}
--- include/task2.h
+++ include/task2.h
@@ -1,27 +1,8 @@
 #include <iostream>
+#include "task.h"
 using namespace std;
 
 
-/*template<class T>
-T gen()
-{
-	static int t = 48;
-	return t++;
-}
-
-template<>
-char* gen()
-{
-	static unsigned t = 0;
-	t++;
-	char* s = new char[t + 1];
-
-	for (size_t i = 0; i < t; i++)
-		s[i] = '0' + i;
-	s[t] = '\0';
-
-	return s;
-}*/
 
 template <typename T, size_t n>
 T* createArr(T (*gen)())
