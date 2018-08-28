11eb0b8599294d6b7a7b0363ec0a769f8cbe4680
MashaSS <33683640+MashaSS@users.noreply.github.com>
2018-08-28 21:26:19 +0300

    Update task.h

--- include/task.h
+++ include/task.h
@@ -1,33 +1,14 @@
 #ifndef _TASK_H_
 #define _TASK_H_
 
-template<typename T> T gen()
+template<typename T> 
+T gen()
 {
-	return 0;
-}
-template<> int gen()
-{
-	int res = rand() % 100;
-	return res;
-}
-template<> double gen()
-{
-	double res = rand() % 1000 / 10;
-	return res;
+	return rand() % 100;
 }
 
-template<> float gen()
-{
-	float res = rand() % 1000 / 10;
-	return res;
-}
-
-template<> char gen()
-{
-	return 'a' + rand() % 24;
-}
-
-template<> char* gen()
+template<> 
+char* gen()
 {
 	const int n = rand() % 10 + 1;
 	char *res = new char[n];
@@ -37,41 +18,14 @@ template<> char* gen()
 	return res;
 }
 
-
-template<> bool gen()
-{
-	return rand() / 2 %10;
-}
-
 template<typename T>
  T change(T a)
 {
 	return a + 2;
 }
 
-
-template<> int change(int a)
-{
-	return a * a;
-}
-
-template<> float change(float a)
-{
-	return a + 1.3;
-}
-
-template<> double change(double a)
-{
-	return a * 2;
-}
-
-template<> char change(char a)
-{
-	return a + 2;
-}
-
-
-template<> char* change(char* a)
+template<> 
+char* change(char* a)
 {
 	for (int i = 0; i < strlen(a); i++)
 		a[i] = a[i] + 2;
