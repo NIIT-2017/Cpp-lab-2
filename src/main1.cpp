968b6ac991aa26d55f64afb66e7cd73ab1905429
MashaSS <33683640+MashaSS@users.noreply.github.com>
2018-08-28 18:39:01 +0300

    update

--- src/main1.cpp
+++ src/main1.cpp
@@ -10,17 +10,17 @@ int main()
 	double a3[7] = { 90.0, 5.6, 74.5, 4.5, 6.44, 3.6, 2.3 };
 	char* a4[7]{ (char*)"wqhfktf", (char*)"qgjrf", (char*)"d", (char*)"afhk", (char*)"ajkwefljqv", (char*)"oo" ,(char*)"ooss" };
 	
-	MergeSort(a1, 0, 6); 
+	msort(a1, 0, 6); 
 	for (i = 0; i < 7; i++) cout << a1[i] << " ";
 	cout << endl;
-	MergeSort(a2, 0, 6);
+	msort(a2, 0, 6);
 	for (i = 0; i < 7; i++) cout << a2[i] << " ";
 	cout << endl;
-	MergeSort(a3, 0, 6);
+	msort(a3, 0, 6);
 	for (i = 0; i < 7; i++) cout << a3[i] << " ";
 	cout << endl;
 	
-	MergeSort(a4, 0, 6);
+	msort(a4, 0, 6);
 	for (i = 0; i < 7; i++) cout << a4[i] << " ";
 	cout << endl;
 	return 0;
