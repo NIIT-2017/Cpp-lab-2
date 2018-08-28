4b076e8fb0121ff14dee8c730a7282650f7c35ed
MashaSS <33683640+MashaSS@users.noreply.github.com>
2018-08-28 22:28:17 +0300

    Update task2.h

--- include/task2.h
+++ include/task2.h
@@ -1,8 +1,8 @@
 #ifndef _TASK2_H_
 #define _TASK2_H_
 
-template <typename T>
-T* createArr(const int N, T(*gen)())
+template <typename T, int N>
+T* createArr(T (*gen)())
 {
 	T *arr = new T[N];
 	for (int i = 0; i < N; i++)
