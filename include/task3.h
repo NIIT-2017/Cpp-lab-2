31e305445e4d2e1fd5d699f05a6b8a392479faa2
MashaSS <33683640+MashaSS@users.noreply.github.com>
2018-08-28 19:47:25 +0300

    Update task3.h

--- include/task3.h
+++ include/task3.h
@@ -1,32 +1,4 @@
-template<typename T> T change(T);
-
-template<> int change(int a)
-{
-	return a * a;
-}
-
-template<> float change(float a)
-{
-	return a + 1.3;
-}
-
-template<> double change(double a)
-{
-	return a * 2;
-}
-
-template<> char change(char a)
-{
-	return a + 2;
-}
-
-
-template<> char* change(char* a)
-{
-	for (int i = 0; i < strlen(a); i++)
-		a[i] = a[i] + 2;
-	return a;
-}
+#include"task.h"
 
 template<typename T>
 void map(T *arr, int n, T(*change)(T))
