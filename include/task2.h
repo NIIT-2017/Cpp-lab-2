4d70e434c7afcf892031572aec6f24f5623733d2
asbelov <asbelov@NPC-101-NB.LEMZ.LOCAL>
2018-09-06 12:39:34 +0300

    delete errors

--- include/task2.h
+++ include/task2.h
@@ -1,24 +1,17 @@
 #ifndef TASK2_H
 #define TASK2_H
 #include <cstring>
-#include<iostream>
 
-template<class T, size_t size> T* createArr(T(*gen)()) {
+template<class T, size_t size> T* createArr(T(*gen)()) 
+{
 	T *arr = new T[size];
 	T val = 0;
-	
-	for (size_t i = 0; i < size; i++) 
+
+	for (size_t i = 0; i < size; i++)
 	{
 		val = gen();
 		arr[i] = val;
 	}
-
-	for (size_t i = 0; i < size; i++) 
-	{
-		std::cout << arr[i] << " ";
-	}
-
-	delete[] arr;
 	return arr;
 }
 #endif		//TASK2_H
--- src/main2.cpp
+++ src/main2.cpp
@@ -3,19 +3,22 @@
 #include"task.h"
 #define SIZE 7
 
-int main() 
+int main()
 {
 	int *arr1;
 	arr1 = createArr<int, SIZE>(gen);
-	std::cout << std::endl;
+	for (size_t i = 0; i < SIZE; i++)
+		std::cout << arr1[i] << " ";
 
 	char *arr2;
 	arr2 = createArr<char, SIZE>(gen);
-	std::cout << std::endl;
+	for (size_t i = 0; i < SIZE; i++)
+		std::cout << arr2[i] << " ";
 
 	char **arr3;
 	arr3 = createArr<char*, SIZE>(gen);
-	std::cout << std::endl;
+	for (size_t i = 0; i < SIZE; i++)
+		std::cout << arr3[i] << " ";
 
 	return 0;
 }
